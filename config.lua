e9ac41fa57d40e658a70e950573b316ddbe97a32
RexShack <rexshackuk@gmail.com>
2022-06-04 06:25:00 +0100

    Update config.lua
    changed to the default jobs in qbr-core framework

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
 Config.LawAlertTime = 300000 -- 5 mins
-Config.LawmanJob = 'lawman' -- job name for the lawman
+Config.LawmanJob = 'police' -- job name for the police/lawman
 Config.LawBlipHash = 1095891419 -- blip hash
 
 Config.MedicAlertTime = 300000 -- 5 mins
-Config.MedicJob = 'medic' -- job name for the lawman
+Config.MedicJob = 'ambulance' -- job name for the ambulance/medic
 Config.MedicBlipHash = 1451797164 -- blip hash
